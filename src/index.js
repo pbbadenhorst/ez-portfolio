b80a9ecac8ac1eea24d68922b6b639cade76fa48
pbbadenhorst <pbbadenhorst@users.noreply.github.com>
2018-12-29 15:43:11 -0800

    Create sqlite3 database with migration table

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database("data.sqlite")
+
+db.run(`CREATE TABLE IF NOT EXISTS _migrations
+(
+    id integer PRIMARY KEY,
+    name text UNIQUE
+)`)
+
 require('http')
     .createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "text/html" })
         res.end(`<h1>Hello world</h1><p>The current time is ${new Date().toLocaleString()}</p>`)
     })
     .listen(process.env.PORT || 8080)
+
